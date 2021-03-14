c25f2c0b54759402e96450322b3b458d95c04eeb
José Pabón <jose.pabon@bringglobal.com>
2021-03-14 11:44:55 -0500

    Initial commit

---
+++ Pokedex-CL/Pokedex_CLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_CLApp.swift
+//  Pokedex-CL
+//
+//  Created by Jose Javier Pabon Granados on 13/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_CLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
