b1003b012386f0060ad47d9b24950f043d3ad81f
José Pabón <jose.pabon@bringglobal.com>
2021-03-14 12:43:52 -0500

    feat: Added Pokemon model

---
+++ Pokedex-CL/Model/Pokemon.swift
@@ -0,0 +1,305 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let pokemon = try? newJSONDecoder().decode(Pokemon.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Pokemon
+struct Pokemon: Codable {
+    let baseExperience: Int
+    let gameIndices: [GameIndex]
+    let height, id: Int
+    let moves: [Move]
+    let name: String
+    let order: Int
+    let sprites: Sprites
+    let stats: [Stat]
+    let types: [TypeElement]
+    let weight: Int
+
+    enum CodingKeys: String, CodingKey {
+        case baseExperience = "base_experience"
+        case gameIndices = "game_indices"
+        case height, id, moves, name, order, sprites, stats, types, weight
+    }
+}
+
+// MARK: - GameIndex
+struct GameIndex: Codable {
+    let gameIndex: Int
+    let version: Version
+
+    enum CodingKeys: String, CodingKey {
+        case gameIndex = "game_index"
+        case version
+    }
+}
+
+// MARK: - Version
+struct Version: Codable {
+    let name: String
+    let url: String
+}
+
+// MARK: - Move
+struct Move: Codable {
+    let move: Version
+}
+
+// MARK: - Sprites
+struct Sprites: Codable {
+    let frontDefault: String
+    let other: Other
+    let versions: Versions
+
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case other, versions
+    }
+}
+
+// MARK: - Other
+struct Other: Codable {
+    let officialArtwork: OfficialArtwork
+
+    enum CodingKeys: String, CodingKey {
+        case officialArtwork = "official-artwork"
+    }
+}
+
+// MARK: - OfficialArtwork
+struct OfficialArtwork: Codable {
+    let frontDefault: String
+
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
+
+// MARK: - Versions
+struct Versions: Codable {
+    let generationI: GenerationI
+    let generationIi: GenerationIi
+    let generationIii: GenerationIii
+    let generationIv: GenerationIv
+    let generationV: GenerationV
+    let generationVi: [String: GenerationVi]
+    let generationVii: GenerationVii
+    let generationViii: GenerationViii
+
+    enum CodingKeys: String, CodingKey {
+        case generationI = "generation-i"
+        case generationIi = "generation-ii"
+        case generationIii = "generation-iii"
+        case generationIv = "generation-iv"
+        case generationV = "generation-v"
+        case generationVi = "generation-vi"
+        case generationVii = "generation-vii"
+        case generationViii = "generation-viii"
+    }
+}
+
+// MARK: - GenerationI
+struct GenerationI: Codable {
+    let redBlue, yellow: RedBlue
+
+    enum CodingKeys: String, CodingKey {
+        case redBlue = "red-blue"
+        case yellow
+    }
+}
+
+// MARK: - RedBlue
+struct RedBlue: Codable {
+    let backDefault, backGray, frontDefault, frontGray: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case backGray = "back_gray"
+        case frontDefault = "front_default"
+        case frontGray = "front_gray"
+    }
+}
+
+// MARK: - GenerationIi
+struct GenerationIi: Codable {
+    let crystal, gold, silver: Crystal
+}
+
+// MARK: - Crystal
+struct Crystal: Codable {
+    let backDefault, backShiny, frontDefault, frontShiny: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case backShiny = "back_shiny"
+        case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
+    }
+}
+
+// MARK: - GenerationIii
+struct GenerationIii: Codable {
+    let emerald: Emerald
+    let fireredLeafgreen, rubySapphire: Crystal
+
+    enum CodingKeys: String, CodingKey {
+        case emerald
+        case fireredLeafgreen = "firered-leafgreen"
+        case rubySapphire = "ruby-sapphire"
+    }
+}
+
+// MARK: - Emerald
+struct Emerald: Codable {
+    let frontDefault, frontShiny: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
+    }
+}
+
+// MARK: - GenerationIv
+struct GenerationIv: Codable {
+    let diamondPearl, heartgoldSoulsilver, platinum: DiamondPearl
+
+    enum CodingKeys: String, CodingKey {
+        case diamondPearl = "diamond-pearl"
+        case heartgoldSoulsilver = "heartgold-soulsilver"
+        case platinum
+    }
+}
+
+// MARK: - DiamondPearl
+class DiamondPearl: Codable {
+    let backDefault: String
+    let backFemale: JSONNull?
+    let backShiny: String
+    let backShinyFemale: JSONNull?
+    let frontDefault: String
+    let frontFemale: JSONNull?
+    let frontShiny: String
+    let frontShinyFemale: JSONNull?
+    let animated: DiamondPearl?
+
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case backFemale = "back_female"
+        case backShiny = "back_shiny"
+        case backShinyFemale = "back_shiny_female"
+        case frontDefault = "front_default"
+        case frontFemale = "front_female"
+        case frontShiny = "front_shiny"
+        case frontShinyFemale = "front_shiny_female"
+        case animated
+    }
+
+    init(backDefault: String, backFemale: JSONNull?, backShiny: String, backShinyFemale: JSONNull?, frontDefault: String, frontFemale: JSONNull?, frontShiny: String, frontShinyFemale: JSONNull?, animated: DiamondPearl?) {
+        self.backDefault = backDefault
+        self.backFemale = backFemale
+        self.backShiny = backShiny
+        self.backShinyFemale = backShinyFemale
+        self.frontDefault = frontDefault
+        self.frontFemale = frontFemale
+        self.frontShiny = frontShiny
+        self.frontShinyFemale = frontShinyFemale
+        self.animated = animated
+    }
+}
+
+// MARK: - GenerationV
+struct GenerationV: Codable {
+    let blackWhite: DiamondPearl
+
+    enum CodingKeys: String, CodingKey {
+        case blackWhite = "black-white"
+    }
+}
+
+// MARK: - GenerationVi
+struct GenerationVi: Codable {
+    let frontDefault: String
+    let frontFemale: JSONNull?
+    let frontShiny: String
+    let frontShinyFemale: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case frontFemale = "front_female"
+        case frontShiny = "front_shiny"
+        case frontShinyFemale = "front_shiny_female"
+    }
+}
+
+// MARK: - GenerationVii
+struct GenerationVii: Codable {
+    let icons: Icons
+    let ultraSunUltraMoon: GenerationVi
+
+    enum CodingKeys: String, CodingKey {
+        case icons
+        case ultraSunUltraMoon = "ultra-sun-ultra-moon"
+    }
+}
+
+// MARK: - Icons
+struct Icons: Codable {
+    let frontDefault: String
+    let frontFemale: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case frontFemale = "front_female"
+    }
+}
+
+// MARK: - GenerationViii
+struct GenerationViii: Codable {
+    let icons: Icons
+}
+
+// MARK: - Stat
+struct Stat: Codable {
+    let baseStat, effort: Int
+    let stat: Version
+
+    enum CodingKeys: String, CodingKey {
+        case baseStat = "base_stat"
+        case effort, stat
+    }
+}
+
+// MARK: - TypeElement
+struct TypeElement: Codable {
+    let slot: Int
+    let type: Version
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
