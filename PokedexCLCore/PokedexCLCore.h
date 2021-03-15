c89902874f6190e9282e60ccb9bc727a182f010c
José Pabón <jose.pabon@bringglobal.com>
2021-03-15 09:26:57 -0500

    chore: Moved core logic to its own framework

---
+++ PokedexCLCore/PokedexCLCore.h
@@ -0,0 +1,18 @@
+//
+//  PokedexCLCore.h
+//  PokedexCLCore
+//
+//  Created by Jose Javier Pabon Granados on 15/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokedexCLCore.
+FOUNDATION_EXPORT double PokedexCLCoreVersionNumber;
+
+//! Project version string for PokedexCLCore.
+FOUNDATION_EXPORT const unsigned char PokedexCLCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokedexCLCore/PublicHeader.h>
+
+
